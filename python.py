09cd16147dac66e6b1c20034703e8ff26ac22254
ruhalaarav <112325488+ruhalaarav@users.noreply.github.com>
2023-06-17 16:44:10 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,12 @@
+from tkinter import *
+window = Tk()
+
+window.title("Driving license")
+Label("Id = 78453")
+Label("Name: Aarav")
+Label("Date Of Birth: 20.3.2002")
+Label("Pin No. : 7418")
+Label("Adress: Disney Land Hong Kong")
+
+
+window.mainloop()
